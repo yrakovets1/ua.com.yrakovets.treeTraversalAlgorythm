8278714f9c9151bc41f6fcf7daa7ed76e3a28045
yrakovets <yurii.rakovets@microfocus.com>
2018-12-05 16:04:56 +0200

    #0.8

--- src/test/java/testcoin/CoinTest.java
+++ src/test/java/testcoin/CoinTest.java
@@ -13,9 +13,9 @@ public class CoinTest {
 
     @Test
     public void simpleTest2(){
-        CoinAlgorythm alg = new CoinAlgorythm(93);
+        CoinAlgorythm alg = new CoinAlgorythm(81);
         alg.run();
-        Assert.assertEquals(" 50 25 10 5 2 1", alg.print());
+        Assert.assertEquals(" 50 10 10 10 2 1", alg.print());
     }
 
 }
